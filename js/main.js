328503de556567486f53a100997779935cf31469
Dean Vo <deaanvo@gmail.com>
2020-10-26 15:48:09 -0700

    assign form values into the profile data model when form is submitted #1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var elAvatarInput = document.querySelector('#avatar-url');
+var elSave = document.querySelector('#save-button');
+var elInputs = document.querySelectorAll('input');
+var elBio = document.querySelector('textarea');
 
 function avatarInput() {
   var avatarImg = document.querySelector('.avatar-placeholder');
@@ -6,3 +9,13 @@ function avatarInput() {
 }
 
 elAvatarInput.addEventListener('input', avatarInput);
+
+function saveButton() {
+  data.profile.avatarUrl = elInputs[0].value;
+  data.profile.username = elInputs[1].value;
+  data.profile.fullName = elInputs[2].value;
+  data.profile.location = elInputs[3].value;
+  data.profile.bio = elBio.value;
+}
+
+elSave.addEventListener('click', saveButton);
