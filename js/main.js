5e4ff68f7e2a796d5166bf534e46ea77463abd44
Dean Vo <deaanvo@gmail.com>
2020-10-26 19:57:01 -0700

    had to go back and retweek code. added form element and used submit event instead of click. forms and image are reset, and user data is stored between reloads. #1

--- js/main.js
+++ js/main.js
@@ -1,21 +1,31 @@
 var elAvatarInput = document.querySelector('#avatar-url');
-var elSave = document.querySelector('#save-button');
 var elInputs = document.querySelectorAll('input');
 var elBio = document.querySelector('textarea');
+var elForm = document.querySelector('form');
+var elImage = document.querySelector('.avatar-placeholder');
 
 function avatarInput() {
-  var avatarImg = document.querySelector('.avatar-placeholder');
-  avatarImg.src = elAvatarInput.value;
+  elImage.src = elAvatarInput.value;
 }
 
 elAvatarInput.addEventListener('input', avatarInput);
 
 function saveButton() {
+  event.preventDefault();
+
   data.profile.avatarUrl = elInputs[0].value;
   data.profile.username = elInputs[1].value;
   data.profile.fullName = elInputs[2].value;
   data.profile.location = elInputs[3].value;
   data.profile.bio = elBio.value;
+
+  elForm.reset();
+  elImage.src = 'images/placeholder-image-square.jpg';
 }
 
-elSave.addEventListener('click', saveButton);
+elForm.addEventListener('submit', saveButton);
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal', dataJSON);
+});
